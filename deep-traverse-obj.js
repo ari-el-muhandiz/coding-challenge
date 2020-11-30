b6f2740ee4f897189cd2d3c3bd83ebf3cf2a3232
Ari Firmanto <ari@waresix.com>
2020-11-30 17:19:11 +0700

    fix algorithm object checker

--- deep-traverse-obj.js
+++ deep-traverse-obj.js
@@ -7,6 +7,33 @@ const obj = {
         f: {
             g: {
                 h: {
+                    i: 2,
+                    j: {
+                        k: 1,
+                        l: 1,
+                        m: {
+                            n: 1,
+                            o: 1
+                        }
+                    }
+                }
+            }
+        }
+    },
+    p: 1
+}
+
+const obj1 = {
+    a: {
+        k: 1
+    },
+    b: 1,
+    c: {
+        d: 1,
+        e: 1,
+        f: {
+            g: {
+                h: {
                     i: 1,
                     j: 1
                 }
@@ -15,24 +42,34 @@ const obj = {
     }
 }
 
+const objeckChecker = (obj) => {
+  let valKey = '';
+  const iterate = (obj) => {
+    let isSame = true;
 
-const iterate = (obj, initValue = '') => {
-  let isSame = true;
-  for (let index = 0; index < Object.keys(obj).length; index++) {
-      const elmKey = Object.keys(obj)[index];
-      const currRes = obj[elmKey]
+    for (let index = 0; index < Object.keys(obj).length; index++) {
+        const elmKey = Object.keys(obj)[index];
+        const currRes = obj[elmKey]
+      
+      if (typeof currRes === 'object') {
+         isSame = iterate(currRes);
+      } else {
+          if (valKey === '') {
+              valKey = currRes;
+          } else if (valKey !== currRes) {
+              return false;
+          }
+      } 
+      if (!isSame) {
+        break;          
+      }
    
-    if (typeof currRes === 'object') {
-       isSame = iterate(currRes, initValue);
-    } else {
-        if (initValue === '') {
-            initValue = currRes;
-        } else if (initValue !== currRes) {
-            return false;
-        }
-    }    
+    }
+    return isSame;
   }
-  return isSame;
+
+  return iterate(obj);
 }
 
-console.log(iterate(obj));
+console.log(objeckChecker(obj));
+console.log(objeckChecker(obj1));
